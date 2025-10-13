aff578ecbc0d5c1a3b650c5e59ef84cf27f92109
archkrrr <163066042+archkrrr@users.noreply.github.com>
2025-10-13 18:25:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { extension_settings, getContext } from "../../../extensions.js";
-import { saveSettingsDebounced, event_types, eventSource } from "../../../../script.js";
+import { saveSettingsDebounced, event_types, eventSource, setVariable } from "../../../../script.js";
 import { executeSlashCommandsOnChatInput, registerSlashCommand } from "../../../slash-commands.js";
 
 const extensionName = "SillyTavern-CostumeSwitch-Testing";
