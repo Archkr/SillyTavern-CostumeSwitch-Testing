1460a62da88104e0044285a3fe13159acc72f081
archkrrr <163066042+archkrrr@users.noreply.github.com>
2025-10-28 21:30:13 -0500

    Delete src directory

--- src/dependencies.js
+++
@@ -1,3 +0,0 @@
-export { extension_settings, getContext } from "../../../../extensions.js";
-export { saveSettingsDebounced, event_types, eventSource } from "../../../../../script.js";
-export { executeSlashCommandsOnChatInput, registerSlashCommand } from "../../../../slash-commands.js";
